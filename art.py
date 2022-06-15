f21145859e5cef2d63e9e922dcd0a77b8cd993be
Ernie <hello@ernie.sg>
2022-06-16 02:15:00 +0800

    updated art.py

--- art.py
+++ art.py
@@ -4,20 +4,17 @@ import io
 import urllib.request
 import urllib.parse
 import requests
-import ipdb; 
 
 BASE_URI = "https://collectionapi.metmuseum.org/public/collection/v1/"
 
 def emo_search(query):
-    '''Look for a given city and disambiguate between several candidates. Return one city (or None)'''
+    '''Look for a given work based on user query'''
     url = BASE_URI + "search?q=" + query
     response = requests.get(url).json()
     artwork_lst = response['objectIDs']
     for i, item in enumerate(artwork_lst):
         art_url = BASE_URI + "objects/" + str(item)
         art_response = requests.get(art_url).json()
-#        if i == 10:
-#            break
         if art_response['constituents'] is not None and art_response['primaryImage']:
             print(f"{art_response['title']} by : {art_response['constituents'][0]['name']} at {art_response['primaryImage']}")
             img_response = requests.get('https://picsum.photos/seed/picsum/200/300')
